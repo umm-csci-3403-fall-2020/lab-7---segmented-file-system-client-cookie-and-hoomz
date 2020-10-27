e9bef7a1297dfb8fb5c963dc7e2758146dd2d321
Cookie Vang <vang2660@morris.umn.edu>
2020-10-27 11:20:02 -0500

    Create and start implementing datagram socket

---
+++ src/client.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.netInetAddress;
+import java.util.Arrays;
+
+public class dataSocket {
+
+	public static void main(String[] args) throws IOException {
+		DatagramSocket socket = new DatagramSocket();
+		InetAddress address = InetAddress.getByName("localhost");
+		int port = 6013;
+		byte[] buffer = new byte[65507]; //max size of possible things returned
+		byte[] buffer2 = new byte[65507];
+		socket.setSoTimeout(3000); //times out and closes client if nothing is returned
+
+		DatagramPacket blank = new DatagramPacket(buffer,0,address,port); //sending a blank packet to the server
+		DatagramPacket blank = new DatagramPacket(
+
+	}
+
+}
