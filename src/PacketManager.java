5171e7d5393b65a2b5971aa1b86d82aa844eb489
Cookie Vang <vang2660@morris.umn.edu>
2020-10-29 12:48:37 -0500

    Add FileReceiver and PacketManager classes

---
+++ src/PacketManager.java
@@ -0,0 +1,4 @@
+public class PacketManager {
+    //stuff here to determine whether a packet is a header or data packet,
+    //then constructs the packet and adds it to the correct ReceivedFile object
+}
